3e71ac4262aef106a58f06a972f585efa704cea8
NASIR AKHTAR <131844398+nasirakhtarj123@users.noreply.github.com>
2024-01-16 11:44:32 +0530

    Add files via upload

---
+++ JenkinsApplication.java
@@ -0,0 +1,21 @@
+package SpringDeveopsIntegration.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsApplication {
+	@GetMapping("/get")
+	public String getMessage(){
+
+		return "Welcome to Docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
